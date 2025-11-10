e4c0f71f7ad255f230f655659cb1e4c8c2c6c12d
Felipe Eguia <felipeeguia03@gmail.com>
2025-11-10 12:37:03 -0300

    Fix: Agregar CORS para Render frontend

--- final/backend/main.go
+++ final/backend/main.go
@@ -16,11 +16,12 @@ func normalize(s string) string { return strings.TrimRight(s, "/") }
 func main() {
 	clients.StartDB()
 
-	// Whitelist de orígenes permitidos (QA, PROD y dev local)
+	// Whitelist de orígenes permitidos (QA, PROD, Render y dev local)
 	allowedOrigins := map[string]struct{}{
 		"http://localhost:3000": {},
 		"https://webapp-tp05-front-qa-verzini-eguia-b6h9asc9g5axc6hz.chilecentral-01.azurewebsites.net":   {},
 		"https://webapp-tp05-front-prod-verzini-eguia-cjdkf2dac9g3g2as.chilecentral-01.azurewebsites.net": {},
+		"https://tp8-frontend.onrender.com": {},
 	}
 
 	engine := gin.New()
